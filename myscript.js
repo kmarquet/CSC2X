0aec8dcdca5e69aa3d3cf3a8f000150ea6b3f848
Kevin Marquet <Kevin.Marquet@gmail.com>
2017-02-19 17:46:45 +0100

    Added cross-references handling

--- myscript.js
+++ myscript.js
@@ -1,154 +1,164 @@
-var fs = require('fs');  
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+}
 
-var txt = "";
-
-var curricula = JSON.parse(fs.readFileSync('essai.json').toString());
-
-txt += "<table>\n";
-for (var area in curricula) {
-    areaName = curricula[area].area_name;
-    txt += "<tr>\n"; 
-    txt += "<th valign=\"top\" align=\"left\">\n"; 
-    txt += "<div class=\"areatitle\" id=\"" + areaName + "\">\n";
-    txt += areaName + "<br>\n";
-    txt += "</div>";
-    txt += "</th>\n"; 
-    txt += "<th valign=\"top\" align=\"left\">\n"; 
-    txt += "<div class=\"areacontent\" id=\"contentof" + areaName + "\">\n";
+window.printCSC = function()
+{
+    
+    var curricula;
+    
+    var fs = require('fs');  
+    
+    var txt = "";
+    
+    curricula = JSON.parse(fs.readFileSync('CSC-content.json').toString());
+    
     txt += "<table>\n";
-    for (var unit in curricula[area].units) {
-        unitName = curricula[area].units[unit].unit_name;
+    for (var area in curricula) {
+        areaName = curricula[area].area_name;
         txt += "<tr>\n"; 
         txt += "<th valign=\"top\" align=\"left\">\n"; 
-        txt += "<div class=\"unittitle\" id=\"" + unitName + "\">\n";
-        txt += unitName + "<br>\n";
-        txt += "</div>\n";
-        txt += "<div class=\"unitcontent\" id=\"contentof" + unitName + "\">\n";
-        var nb = 0;
-        txt += "<table>\n";
-        txt += "<th valign=\"top\" align=\"left\">\n"; 
-        txt += "<p class=\"indent\"></p>";
+        txt += "<div class=\"areatitle\" id=\"" + areaName + "\">\n";
+        txt += areaName + "<br>\n";
+        txt += "</div>";
         txt += "</th>\n"; 
-        txt += "<th valign=\"top\" align=\"left\" class=\"topics\">\n"; 
-        // txt += "<div class=\"topics\">\n";        
+        txt += "<th valign=\"top\" align=\"left\">\n"; 
+        txt += "<div class=\"areacontent\" id=\"contentof" + areaName + "\">\n";
         txt += "<table>\n";
-        for (var topic in curricula[area].units[unit].topics) {
+        for (var unit in curricula[area].units) {
+            unitName = curricula[area].units[unit].unit_name;
+            unitId = areaName + "_" + unitName.replaceAll(" ", "_"); 
+            unitName_woB = unitId.replaceAll("_", " "); 
             txt += "<tr>\n"; 
             txt += "<th valign=\"top\" align=\"left\">\n"; 
-            txt += "<div class=\"topic\">\n";
-            txt += curricula[area].units[unit].topics[topic].topic_content + "\n";
-            txt += "</div>";
+            txt += "<div class=\"unittitle\" id=\"" + unitId + "\">\n";
+            txt += unitName_woB + "<br>\n";
+            txt += "</div>\n";
+            txt += "<div class=\"unitcontent\" id=\"contentof" + unitName + "\">\n";
+            var nb = 0;
             txt += "<table>\n";
-            txt += "<th valign=\"top\" align=\"left\">\n";
-            for (var subtopic in curricula[area].units[unit].topics[topic].subtopics) {
+            txt += "<th valign=\"top\" align=\"left\">\n"; 
+            txt += "<p class=\"indent\"></p>";
+            txt += "</th>\n"; 
+            txt += "<th valign=\"top\" align=\"left\" class=\"topics\">\n"; 
+            // txt += "<div class=\"topics\">\n";        
+            txt += "<table>\n";
+            for (var topic in curricula[area].units[unit].topics) {
+                var topicContent = curricula[area].units[unit].topics[topic].topic_content;
                 txt += "<tr>\n"; 
                 txt += "<th valign=\"top\" align=\"left\">\n"; 
-                txt += "<div class=\"subtopic\"\>\n";            
-                txt += curricula[area].units[unit].topics[topic].subtopics[subtopic].topic_content;
-                txt += "</div>\n";                
+                txt += "<div class=\"topic\">\n";
+                txt += handleTopicContent(topicContent) + "\n";
+                txt += "</div>";
+                txt += "<table>\n";
+                txt += "<th valign=\"top\" align=\"left\">\n";
+                for (var subtopic in curricula[area].units[unit].topics[topic].subtopics) {
+                    txt += "<tr>\n"; 
+                    txt += "<th valign=\"top\" align=\"left\">\n"; 
+                    txt += "<div class=\"subtopic\"\>\n";            
+                    txt += curricula[area].units[unit].topics[topic].subtopics[subtopic].topic_content;
+                    txt += "</div>\n";                
+                    txt += "</th>\n"; 
+                    txt += "<th align=\"left\">\n"; 
+                    txt += "<select class=\"subtopicselect\" onchange=\"this.className=this.options[this.selectedIndex].className\">";
+                    var levels = ["Not", "Addressed"];
+                    for (var i = 0 ; i < levels.length ; i++) {
+                        var level = levels[i];
+                        txt += "<option";
+                        var level_read = curricula[area].units[unit].topics[topic].subtopics[subtopic].addressed;
+                        if (level_read.search(level) >= 0) {
+                            txt += " selected";
+                        }
+                        txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "sub\">";
+                        txt += level;
+                        txt += "</option>";
+                        txt += level_read.search(level);
+                    }
+                    txt += "</select>";
+                    txt += "</th>\n";
+                }
+                txt += "</table>\n";
                 txt += "</th>\n"; 
-                txt += "<th align=\"left\">\n"; 
-                txt += "<select class=\"subtopicselect\" onchange=\"this.className=this.options[this.selectedIndex].className\">";
+                txt += "<th align=\"left\" valign=\"top\">\n"; 
+                txt += "<select onchange=\"this.className=this.options[this.selectedIndex].className\">";
                 var levels = ["Not", "Addressed"];
                 for (var i = 0 ; i < levels.length ; i++) {
                     var level = levels[i];
                     txt += "<option";
-                    var level_read = curricula[area].units[unit].topics[topic].subtopics[subtopic].addressed;
-                    if (level_read.search(level) >= 0) {
+                    if (curricula[area].units[unit].topics[topic].addressed.search(level) >= 0) {
                         txt += " selected";
                     }
-                    txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "sub\">";
+                    txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "\">";
                     txt += level;
                     txt += "</option>";
-                    txt += level_read.search(level);
                 }
                 txt += "</select>";
-                txt += "</th>\n";
+                txt += "</th>\n"; 
             }
             txt += "</table>\n";
+            // txt += "</div>";
             txt += "</th>\n"; 
-            txt += "<th align=\"left\" valign=\"top\">\n"; 
-            txt += "<select onchange=\"this.className=this.options[this.selectedIndex].className\">";
-            var levels = ["Not", "Addressed"];
-            for (var i = 0 ; i < levels.length ; i++) {
-                var level = levels[i];
-                txt += "<option";
-                if (curricula[area].units[unit].topics[topic].addressed.search(level) >= 0) {
-                    txt += " selected";
+            txt += "<th class=\"skills\" align=\"left\" valign=\"top\">\n"; 
+            // txt += "<div class=\"skills\">\n";      
+            txt += "<table>\n";
+            for (var skill in curricula[area].units[unit].skills) {
+                txt += "<tr>\n"; 
+                txt += "<th valign=\"top\" align=\"left\">\n"; 
+                txt += "<div class=\"skill\">\n";
+                txt += curricula[area].units[unit].skills[skill].skill + "\n";
+                txt += "</div>";
+                txt += "</th>\n"; 
+                txt += "<th align=\"left\" valign=\"top\">\n"; 
+                txt += "<select onchange=\"this.className=this.options[this.selectedIndex].className\">";
+                var levels = ["Familiarity", "Usage", "Assessment"];
+                for (var i = 0 ; i < levels.length ; i++) {
+                    var level = levels[i];
+                    txt += "<option";
+                    if (curricula[area].units[unit].topics[topic].addressed.search(level) >= 0) {
+                        txt += " selected";
+                    }
+                    txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "\">";
+                    txt += level;
+                    txt += "</option>";
                 }
-                txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "\">";
-                txt += level;
-                txt += "</option>";
+                txt += "</select>";
+                txt += "</th>\n"; 
             }
-            txt += "</select>";
+            txt += "</table>\n";
+            // txt += "</div>";
             txt += "</th>\n"; 
-        }
-        txt += "</table>\n";
-        // txt += "</div>";
-        txt += "</th>\n"; 
-        txt += "<th class=\"skills\" align=\"left\" valign=\"top\">\n"; 
-        // txt += "<div class=\"skills\">\n";      
-        txt += "<table>\n";
-        for (var skill in curricula[area].units[unit].skills) {
-            txt += "<tr>\n"; 
-            txt += "<th valign=\"top\" align=\"left\">\n"; 
-            txt += "<div class=\"skill\">\n";
-            txt += curricula[area].units[unit].skills[skill].skill + "\n";
+            txt += "</table>\n";
             txt += "</div>";
-            txt += "</th>\n"; 
-            txt += "<th align=\"left\" valign=\"top\">\n"; 
-            txt += "<select onchange=\"this.className=this.options[this.selectedIndex].className\">";
-            var levels = ["Familiarity", "Usage", "Assessment"];
-            for (var i = 0 ; i < levels.length ; i++) {
-                var level = levels[i];
-                txt += "<option";
-                if (curricula[area].units[unit].topics[topic].addressed.search(level) == 0) {
-                    txt += " selected";
-                }
-                txt += " value=\"" + level + "\" class=\"" + level.toLowerCase() + "\">";
-                txt += level;
-                txt += "</option>";
-            }
-            txt += "</select>";
-            txt += "</th>\n"; 
+            
+            // txt += "</th>\n"; 
         }
-        txt += "</table>\n";
-        // txt += "</div>";
-        txt += "</th>\n"; 
         txt += "</table>\n";
         txt += "</div>";
-        
-        // txt += "</th>\n"; 
+        txt += "</th>\n"; 
     }
     txt += "</table>\n";
-    txt += "</div>";
-    txt += "</th>\n"; 
-}
-txt += "</table>\n";
-
-// document.getElementById("demo").innerText = txt;
-document.getElementById("demo").innerHTML = txt;
-
-var allSelects = document.getElementsByTagName("select");
-for (var i=0; i < allSelects.length; i++)
-{
-    allSelects[i].className = allSelects[i].options[allSelects[i].selectedIndex].className;
-}
-
-for (var area in curricula) {
-    var areaName = curricula[area].area_name;
     
-    document.getElementById(areaName).addEventListener("click", function(){displayArea(this.id)});
-//    document.getElementById("demo").innerHTML += areaName + "<br>";
-    for (var unit in curricula[area].units) {
-        unitName = curricula[area].units[unit].unit_name;
-//        document.getElementById("demo").innerHTML += "   " + unitName + " ";
-        document.getElementById(unitName).addEventListener("click", function(){displayUnit(this.id)});
+    document.getElementById("theCSCPage").innerHTML = txt;
+    
+    var allSelects = document.getElementsByTagName("select");
+    for (var i=0; i < allSelects.length; i++)
+    {
+        allSelects[i].className = allSelects[i].options[allSelects[i].selectedIndex].className;
     }
-//    document.getElementById("demo").innerHTML += "<br>";    
+    
+    for (var area in curricula) {
+        var areaName = curricula[area].area_name;
+        
+        document.getElementById(areaName).addEventListener("click", function(){displayArea(this.id)});
+        for (var unit in curricula[area].units) {
+            unitName = curricula[area].units[unit].unit_name;
+            document.getElementById(areaName + "_" + unitName).addEventListener("click", function(){displayUnit(this.id)});
+        }
+    }
 }
 
-function displayArea(areaName) {
+window.displayArea = function(areaName) {
     var element = document.getElementById("contentof" + areaName);
     var eStyle = element.currentStyle ?
         element.currentStyle.display :
@@ -180,16 +190,62 @@ function displayConcept(unitName) {
         document.getElementById(conceptId).style.display = 'none';
 }
 
-    // TODO:
-// - Noms d'unités -> supprimer les '_'
-// - Ajouter les skills
-// - Ajouter les niveaux de connaissance
-// - Support pour la modification des niveaux de connaissances :
-//     - Menus déroulants
-//     - Modification du json
+function handleXrefs(theRefs)
+{
+    var regexList = /([^]*),(\s.{2,4}\/[^]*)/i;
+    var regexEnd = /([^]{2,3})\/([^]*)/i;
+    var match;
+    var theRef;
+    var area;
+    var topic;
+    var res = "";
+    var refs = theRefs.trim();
+    
+    while (regexList.test(refs)) {
+        match = regexList.exec(refs);
+        theRef = match[1].trim();
+        refs = match[2].trim();
+        match = regexEnd.exec(theRef)
+        area = match[1].trim();
+        topic = match[2].trim();
+        res += "<a href=\"\#" + area + "_" + topic.replaceAll(" ", "_") + "\">" + area + "/" + topic + "</a>" + " and ";
+    }
+    match = regexEnd.exec(refs);
+    if (match != null) {
+        area = match[1].trim();
+        topic = match[2].trim();
+        res += "<a href=\"\#" + area + "_" + topic.replaceAll(" ", "_") + "\">" + area + "/" + topic + "</a>";
+    } else {
+        res += "BUG!!!" + refs + "!!!";
+    }
+    return res;
+}
+
+function handleTopicContent(topicContent)
+{
+    var regex = /(.*)\(cross[-\s]reference(.*)\)(.*)/i;
+    var match = regex.exec(topicContent);
+    if (match != null) {
+        var res = "";
+        var i;
+        res += match[1] + " <pre class=crossref>(cross-ref ";
+        res += handleXrefs(match[2]);
+        res += ")</pre>";
+        return res;
+    } else {
+        return topicContent;
+    }
+}
+
+// TODO:
+// - bug: tout est indiqué "non addressed"
+// - Modification du json
 //       fs.writeFile('people.json', JSON.stringify(m));
-// - Rendre ça joli
 // - Ajouter support pour nombre d'heures ??
-// - Ajouter support pour nouveaux topics/skills
 // - Support pour :Xref:
-// - refermer sur 2e clic
+// - Un joli format json:
+//   - indenter
+//   - compléter la recopie depuis le pdf(arg...)
+//       - essayer de copier directement dans le .json
+//       - si c'est trop compliqué => passer par du csv
+//   - les cross-ref sont un peu pénibles à gérer quand il y a des 'and' dans le topic cross-referencé. M'enfin ça se fait.
